1c846d98e99c8be14652e2ab7978c56b486dab8f
Arik Dicks <ariksven@gmail.com>
2021-09-27 17:57:14 -0700

    fixed issues with map

--- dist/src/DataStructures/Map.d.ts
+++ dist/src/DataStructures/Map.d.ts
@@ -4,7 +4,7 @@ declare type IdObj = {
     id: Key;
 };
 interface MapExtension<K, V> {
-    arrayMap<T>(this: Map<K, V>, cb: (val?: V, key?: T) => V): T[];
+    arrayMap<T>(this: Map<K, V>, cb: (val?: V, key?: K) => T): T[];
     toArray(this: Map<K, V>): V[];
     fromArray<T extends IdObj>(this: Map<K, V>, array: T[]): Map<string, T>;
     fromArray<T>(this: Map<K, V>, array: T[], keyName: string): Map<K, V>;
--- src/DataStructures/Map.ts
+++ src/DataStructures/Map.ts
@@ -44,7 +44,7 @@ type IdObj = {
 }
 
 interface MapExtension<K, V> {
-    arrayMap<T>(this: Map<K, V>, cb: (val?: V, key?: T) => V): T[];
+    arrayMap<T>(this: Map<K, V>, cb: (val?: V, key?: K) => T): T[];
     toArray(this: Map<K, V>): V[];
     fromArray<T extends IdObj>(this: Map<K, V>, array: T[]): Map<string, T>;
     fromArray<T>(this: Map<K, V>, array: T[], keyName: string): Map<K, V>;
