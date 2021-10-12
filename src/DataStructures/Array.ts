5514e6f6494ab3f4d6b31bdedb7f77223b3e5482
Arik Dicks <ariksven@gmail.com>
2021-10-11 23:02:16 -0700

    updated

--- src/DataStructures/Array.ts
+++ src/DataStructures/Array.ts
@@ -40,7 +40,7 @@ class Extension<T> {
         }
         return true;
     }
-    remove(this: T[], pred: (value: T, index: number, array: T[]) => void): T {
+    remove(this: T[], pred: (value: T, index: number, array: T[]) => boolean): T {
         let index = this.findIndex(pred);
         let item = this[index];
         this.splice(index, 1);
